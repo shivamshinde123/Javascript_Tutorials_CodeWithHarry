d775d7c83c8aee7d1eeb715907c1a99b4bba120d
Shivam Shinde <shivamshinde92722@gmail.com>
2023-09-01 00:18:01 +0530

    js string methods learned

--- 15_practiceSet4.js
+++ 15_practiceSet4.js
@@ -5,3 +5,20 @@ let my_str = "har\"ry"
 console.log(my_str.length)
 
 // question 2
+const sentence = "The quick brown fox jumps over the lazy dog";
+const word = "fox";
+console.log(sentence.includes(word))
+console.log(`The word ${word} ${sentence.includes(word) ? "is" : "is not"} in the sentence`);
+
+// question 3
+console.log(sentence.toLowerCase())
+console.log(sentence.toUpperCase())
+
+// question 4
+const request = "Please give me Rs 1000."
+let amount = request.slice("Please give me Rs ".length)
+console.log(amount)
+console.log(typeof amount)
+console.log(Number.parseInt(amount))
+
+
