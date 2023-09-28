614b3af255c843a4eb97946ffcbdf5a83cfbf155
Shivam Shinde <shivamshinde92722@gmail.com>
2023-09-28 14:43:09 +0530

    array methods learned

---
+++ 16_Introduction_to_Arrays.js
@@ -0,0 +1,25 @@
+
+// creating an array (can have values with different datatypes)
+let marks_class_12 = [91, 82, 63, 84, null]
+console.log(marks_class_12)
+
+// accessing the values of an array
+console.log(marks_class_12[1])
+console.log(marks_class_12[2])
+console.log(marks_class_12[0])
+console.log(marks_class_12[10]) // this will give the undefined value since 10 index is not present in the array 
+
+// finding the length of an array
+console.log(marks_class_12.length)
+
+// adding the new element to an array 
+marks_class_12[5] = 100;
+console.log(marks_class_12) 
+
+// changing the value of an array (arrays are mutable)
+marks_class_12[2] = 77;
+console.log(marks_class_12)
+
+// typeof array --> arrays are object type (DOUBT)
+console.log(typeof marks_class_12)
+
---
+++ 17_array_methods.js
@@ -0,0 +1,28 @@
+
+
+let num = [1, 2, 3, 4, 5]
+
+// converting an array to string
+let b = num.toString()
+console.log(b)
+
+// joining array elements with separator
+let c = num.join(" and ")
+console.log(c)
+console.log(typeof c)
+
+// popping last element from an array and updating the original array
+let r = num.pop() // pop returns the popped element
+console.log(num, r)
+
+// adding the element at last 
+let n = num.push(34) // push return the new array length
+console.log(num, n)
+
+// removing the first element and then returning it 
+let p = num.shift() // shift returns the removed element
+console.log(num, p)
+
+// add the element at the beginning of an array and then returning the length of an array
+let k = num.unshift(45) // returns the new array length
+console.log(num, k)
